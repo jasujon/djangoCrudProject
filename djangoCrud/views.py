fe88dbec56c765b56baa015ec8d11cac84b61b83
jasujon <jubayedalamsujon@gmail.com>
2020-04-18 12:46:45 +0600

    all booklist show in index.py & all data send djangoCrud->views.py

--- djangoCrud/views.py
+++ djangoCrud/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import BookList
 
 # Create your views here.
 
 def index(request):
-    return render(request , 'index.html')
+    books = BookList.objects.all()
+    return render(request , 'index.html',{'books':books})
