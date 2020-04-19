31811cd1ec6c93715a5507ec76752f0648460bfb
jasujon <jubayedalamsujon@gmail.com>
2020-04-19 22:07:03 +0600

    Data Store Complite

--- djangoCrud/views.py
+++ djangoCrud/views.py
@@ -18,3 +18,12 @@ def delete(request , id):
 
 def add_book(request):
     return render(request , 'add_book.html')
+
+def create(request):
+    print(request.POST)
+    title = request.GET['title']
+    price = request.GET['price']
+    author= request.GET['author']
+    book_details = BookList(title = title , price = price , author = author)
+    book_details.save()
+    return redirect('/') 
